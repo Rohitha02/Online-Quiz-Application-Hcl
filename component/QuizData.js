01dec304cd59fbd48ce90d18ba6b2997f4f5c205
Rohitha02 <134839329+Rohitha02@users.noreply.github.com>
2023-05-28 20:44:53 +0530

    Add files via upload

---
+++ component/QuizData.js
@@ -0,0 +1,93 @@
+export const QuizData=[
+    {
+        question:"What is React.js ?",
+        answer:[
+            {ans:"Open-source JavaScript back-end library ",isCorrect:false},
+            {ans:"JavaScript front-end library to create a database",isCorrect:false},
+            {ans:"Free and open-source JavaScript front-end library",isCorrect:true},
+            {ans:"None of the mentioned",isCorrect:false}
+        ]
+    },
+    {
+        question:"Which of the following acts as the input of a class-based component ?",
+        answer:[
+            {ans:"Class ",isCorrect:false},
+            {ans:"Props",isCorrect:true},
+            {ans:"Factory",isCorrect:false},
+            {ans:"None of the mentioned",isCorrect:false}
+        ]
+    },
+    {
+        question:"React.js is written in which of the following language ?",
+        answer:[
+            {ans:"Java Script ",isCorrect:true},
+            {ans:"C",isCorrect:false},
+            {ans:"C++",isCorrect:false},
+            {ans:"Java",isCorrect:false}
+        ]
+    },
+    {
+        question:"How many elements can a valid react component return ?",
+        answer:[
+            {ans:"1 element ",isCorrect:true},
+            {ans:"React doesn’t return element",isCorrect:false},
+            {ans:"More than 1 element",isCorrect:false},
+            {ans:" None of the mentioned",isCorrect:false}
+        ]
+    },
+    {
+        question:"In which of the following directory React Components are saved ?",
+        answer:[
+            {ans:"Inside components/js/",isCorrect:false},
+            {ans:"Inside vendor/js/components/",isCorrect:false},
+            {ans:"Inside js/components/",isCorrect:true},
+            {ans:"Inside vendor/components/",isCorrect:false}
+        ]
+    },
+    {
+        question:"Which of the following command is used to Install create-react-app ?",
+        answer:[
+            {ans:"npm install create-react-app",isCorrect:false},
+            {ans:"npm install -f create-react-app",isCorrect:false},
+            {ans:"npm install -g create-react-app",isCorrect:true},
+            {ans:"install -g create-react-app",isCorrect:false}
+        ]
+    },
+    {
+        question:"Which of the following is method is not a part of ReactDOM ?",
+        answer:[
+            {ans:"ReactDOM.hydrate() ",isCorrect:false},
+            {ans:"ReactDOM.destroy()",isCorrect:true},
+            {ans:"ReactDOM.createPortal()",isCorrect:false},
+            {ans:"All of the above",isCorrect:false}
+        ]
+    },
+    {
+        question:"Which of the following is the correct name of React.js ?",
+        answer:[
+            {ans:"React",isCorrect:false},
+            {ans:"React.js",isCorrect:false},
+            {ans:"ReactJS",isCorrect:false},
+            {ans:"All the above",isCorrect:true}
+        ]
+    },
+    {
+        question:" What is the default port where webpack-server runs ?",
+        answer:[
+            {ans:"3000",isCorrect:false},
+            {ans:"8080",isCorrect:true},
+            {ans:"6060",isCorrect:false},
+            {ans:"3030",isCorrect:false}
+        ]
+    },
+    {
+        question:"What are the two ways to handle data in React ?",
+        answer:[
+            {ans:"State & Props",isCorrect:true},
+            {ans:"Services & Components",isCorrect:false},
+            {ans:"State & Services",isCorrect:false},
+            {ans:"State & Component",isCorrect:false}
+        ]  
+    }
+
+]
